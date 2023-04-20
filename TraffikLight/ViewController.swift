b952e951932525966209e53093917b4c2fe76a92
MatveikaSW <mets0n18@vk.com>
2023-04-20 20:30:23 +0300

    Initial Commit

---
+++ TraffikLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraffikLight
+//
+//  Created by Матвей Авдеев on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
